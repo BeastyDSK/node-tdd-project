7d8fc91dff3eea66e67e374e4f41dc8b15b62bbe
krishnaprasanth <dhawansaran14346@gmail.com>
2025-01-14 19:51:27 +0530

    feat(TDD-002): Handle the sum of one or two comma separated numbers

--- src/index.js
+++ src/index.js
@@ -13,7 +13,20 @@ const add = (numStrs) => {
   }
 
   // Handle two numbers in a comma sep string
-  // To be implemented
+  
+  // Split the input by comma to get the numbers as an array
+  const [numOne, numTwo] = numStrs.split(',');
+
+  // Parse the numbers
+  const parsedNumOne = parseInt(numOne);
+  const parsedNumTwo = parseInt(numTwo);
+
+  // If there is no second value return the first value
+  if (isNaN(parsedNumTwo)) {
+    return parsedNumOne;
+  }
+
+  return parsedNumOne + parsedNumTwo;
 }
 
 module.exports = {
--- tests/unit/index.test.js
+++ tests/unit/index.test.js
@@ -17,5 +17,20 @@ describe('When an unexpected input is received', () => {
   });
 });
 
-// To be handled
-describe('When an expected input is received', () => {});
+describe('When an expected input is received', () => {
+  
+  it('should return an invalid number when the input has only one number', () => {
+    expect(add("1")).toBe(1);
+    expect(add("1000")).toBe(1000);
+    expect(add("1000,")).toBe(1000);
+    expect(add("0")).toBe(0);
+    expect(add("0,")).toBe(0);
+  });
+
+  it('should return the sum when the input has two numbers', () => {
+    expect(add("1,2")).toBe(3);
+    expect(add("1,1")).toBe(2);
+    expect(add("0,1")).toBe(1);
+    expect(add("0,0")).toBe(0);
+  });
+});
